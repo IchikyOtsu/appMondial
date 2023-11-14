7e16eec43bf5d729885a9ec08c1343f6bea2a133
hneut202216 <147071100+hneut202216@users.noreply.github.com>
2023-11-14 21:18:12 +0100

    Update ReservationManager.py
    explication des différentes fonction ajouté.

--- utils/class/ReservationManager.py
+++ utils/class/ReservationManager.py
@@ -1,14 +1,30 @@
+
 class ReservationManager:
     def __init__(self):
         self.reservations = []
     def addReservation(self,addRes):
+        '''
+        :param str addRes:newReservation
+        :add newReservation in liste
+        '''
         self.reservations.append(addRes)
-    def affichage(self):
-        for i in self.reservations:
-            print(i)
     def removeReservation(self ,rmvRes):
+        '''
+        :param str rmvRes:liste
+        :remove a name in liste
+        '''
         self.reservations.remove(rmvRes)
     def findReservationByName(self ,nom):
+        '''
+        :param str nom:
+        :find a name in liste
+        '''
         for i in self.reservations:
             if i.nom == nom:
                 return i
+    def affichage(self):
+        '''
+        :print:liste
+        '''
+        for i in self.reservations:
+            print(i)
