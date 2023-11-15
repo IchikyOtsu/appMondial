61c4609a36175cbeda7ac0e853ea93a3f4205fd2
hneut202216 <147071100+hneut202216@users.noreply.github.com>
2023-11-15 17:30:56 +0100

    Update type_cuisine.py
    ajout de id service dans la classe type_cuisine

--- utils/class/type_cuisine.py
+++ utils/class/type_cuisine.py
@@ -2,9 +2,10 @@ class type_cuisine:
     '''
     class used to init a type of cuisine
     '''
-    def __init__(self,id : int ,type : str):
+    def __init__(self,id : int ,type : str,idService : str):
         self._idCuisine = int(id)
         self._nomTypeCuisine = type
+        self._idService = idService
     '''
     :rest: used for security of the attributes
     '''
@@ -21,7 +22,14 @@ class type_cuisine:
     @nomTypeCuisine.setter
     def nomTypeCuisine(self,value):
         self._nomTypeCuisine=value
+    
+    @property
+    def idService(self):
+        return self._idService
+    @idService.setter
+    def idService(self,value):
+        self._idService=value
         
     def __str__(self):
-        return f"Identifiant cuisine : {self.idCuisine}, Type de cuisine:{self.nomTypeCuisine}"
+        return f"Identifiant cuisine : {self.idCuisine}, Type de cuisine:{self.nomTypeCuisine}, Identifiant service : {self.idService}"
 
