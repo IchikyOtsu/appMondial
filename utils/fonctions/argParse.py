d40e4929c6872d47052418742855aa88882f3ebf
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2023-11-15 17:49:05 +0100

    Update argParse.py
    Nbr de client

--- utils/fonctions/argParse.py
+++ utils/fonctions/argParse.py
@@ -11,6 +11,9 @@ def argParse():
     parser.add_argument("-tel", type=int, help="Numéro de téléphone. Exemple: 123456789")
     parser.add_argument("-dh", type=str,
                         help="Date et heure de la réservation (format AAAA-MM-JJ HH:MM). Exemple: '2023-10-10 19:00' Vous devez mettre cette date entre '' ou guillemet. ")
+    parser.add_argument("-nbr", type=int,
+                        help="Nombre de personne : -nbr 15 ")
+
     parser.add_argument("-TC", type=str, help="ID du type de cuisine. Choix : Europe(eu), Asie (asi), Amérique du Nord/Sud (as/an), Afrique (af). Veuillez entrer l'id et non le nom complet.")
     parser.add_argument("-pmr", type=lambda x: (str(x).lower() == 'true'),
                         help="Besoin PMR Choix: (Oui/Nom)")
@@ -22,4 +25,4 @@ def argParse():
     if args.affichage:
         return False
     else:
-        return [args.nom,args.tel,args.dh,args.TC, args.pmr, args.bb]
+        return [args.nom,args.tel,args.dh,args.TC, args.pmr, args.bb,args.nbr]
