d3938d523cba4e041fa947a2bb29088be6eafa8a
Ichiky_Otsu <66952647+Brice66@users.noreply.github.com>
2023-11-22 18:54:19 +0100

    Doc Python de la classe initialisation et de la fonction attribution.

--- utils/classes/initialisation.py
+++ utils/classes/initialisation.py
@@ -6,6 +6,18 @@ import pickle
 from pathlib import Path
 
 class Initialisateur:
+    """
+    Cette classe sert à initialiser et gérer les différents gestionnaires (managers) pour les réservations,
+    les tables, les types de service, et les types de cuisine. Elle permet également de sauvegarder 
+    et de charger ces gestionnaires depuis un fichier.
+
+    Attributs:
+        fichier_sauvegarde (str): Le nom du fichier de sauvegarde.
+        reservation_manager (ReservationManager): Gestionnaire des réservations.
+        table_manager (TableManager): Gestionnaire des tables.
+        typeServManager (TypeServiceManager): Gestionnaire des types de service.
+        typeCuisineManager (type_cuisine_manager): Gestionnaire des types de cuisine.
+    """
     def __init__(self):
         self.fichier_sauvegarde = 'managers.pkl'
         self.reservation_manager = None
@@ -17,16 +29,35 @@ class Initialisateur:
         return f"ReservationManager avec {len(self.reservation_manager.reservations)} réservations"
 
     def initialiser_managers(self):
+        """
+        Initialise les gestionnaires avec de nouvelles instances.
+
+        POST: 
+        - Les gestionnaires de réservations, tables, types de service et types de cuisine sont initialisés.
+        """
         self.reservation_manager = ReservationManager()
         self.table_manager = TableManager()
         self.typeServManager = TypeServiceManager()
         self.typeCuisineManager = type_cuisine_manager()
 
     def sauvegarder_managers(self):
+        """
+        Sauvegarde l'état actuel de tous les gestionnaires dans un fichier.
+
+        POST: 
+        - L'état actuel de l'instance Initialisateur est sauvegardé dans un fichier.
+        """
         with open(self.fichier_sauvegarde, 'wb') as f:
             pickle.dump(self, f)
 
     def charger_ou_initialiser_managers(self):
+        """
+        Charge les gestionnaires à partir d'un fichier de sauvegarde, ou les initialise si le fichier n'existe pas ou est corrompu.
+
+        POST: 
+        - Si le fichier de sauvegarde existe et est valide, les gestionnaires sont chargés à partir de ce fichier.
+        - Si le fichier n'existe pas ou est corrompu, les gestionnaires sont initialisés à nouveau.
+        """
         try:
             if Path(self.fichier_sauvegarde).exists():
                 with open(self.fichier_sauvegarde, 'rb') as f:
--- utils/fonctions/attrib.py
+++ utils/fonctions/attrib.py
@@ -3,6 +3,26 @@ from utils.classes.tableManager import TableManager
 from utils.classes.type_cuisine_manager import type_cuisine_manager
 
 def attribution(list, managerRes, managerTable, managerCuisine):
+    """
+    Attribue une table à une réservation en fonction de la capacité requise, du type de cuisine, et de la disponibilité.
+
+    PRE: 
+    - list: Une liste contenant les détails de la réservation. Elle doit contenir le nom du client, 
+      le numéro de téléphone, la date et l'heure, l'ID du type de cuisine, les besoins en accessibilité PMR,
+      les besoins en chaise bébé et le nombre de personnes.
+    - managerRes: Une instance de ReservationManager pour gérer les réservations.
+    - managerTable: Une instance de TableManager pour gérer les informations des tables.
+    - managerCuisine: Une instance de type_cuisine_manager pour gérer les types de cuisine.
+
+    POST: 
+    - Renvoie la liste originale avec le numéro de la table attribuée ajouté à la fin si une table disponible est trouvée.
+    - Renvoie False si aucune table correspondante n'est disponible.
+    
+    La fonction vérifie chaque table pour s'assurer qu'elle correspond aux critères requis (capacité, type de service)
+    et qu'elle est disponible à la date et l'heure demandées. Si une table correspondante est trouvée,
+    le numéro de cette table est ajouté à la liste de réservation et la liste mise à jour est renvoyée.
+    """
+    
     nomClient, numTel, dateHeure, idTypeCuisine, pmr, bb,nbr = list
     cuisine = managerCuisine.findCuisineByid(idTypeCuisine)
     typeServ = cuisine.idService
